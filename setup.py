73fd70038e8e1ab674a76f0b9ccff3b0f64549ca
Jeppe Rask <jepperaskdk@gmail.com>
2021-05-23 16:30:58 +0200

    Goodbye travis

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pydoctest",
-    version="REPLACED_BY_TRAVIS",
+    version="0.1.1",
     author="Jeppe Rask",
     author_email="jepperaskdk@gmail.com",
     description="Test if doctype types match signatures",
