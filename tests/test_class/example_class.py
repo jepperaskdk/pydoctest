d57eb9a01b20ef0808da0367a324d273e17849d0
Jeppe Rask <jepperaskdk@gmail.com>
2021-05-23 16:20:08 +0200

    Testing pytest and coverage

---
+++ tests/test_class/example_class.py
@@ -0,0 +1,21 @@
+class TestClass():
+    def empty_func(self) -> None:
+        """Empty func
+        """
+        pass
+
+    def func_returns_none(self, a: int) -> None:
+        """Func returns None
+
+        Args:
+            a (int): [description]
+        """
+        pass
+
+    def func_returns_int(self) -> int:
+        """Func returns int
+
+        Returns:
+            int: [description]
+        """
+        pass
