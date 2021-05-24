bb8b0e950b42b46635d42a60e91df99694818330
Jeppe Rask <jepperaskdk@gmail.com>
2021-05-24 11:05:17 +0200

    0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pydoctest",
-    version="0.1.4",
+    version="0.1.5",
     author="Jeppe Rask",
     author_email="jepperaskdk@gmail.com",
     description="Test if doctype types match signatures",
